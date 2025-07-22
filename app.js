780b1406922a94888ed59afee0d3c84aaf414acc
asadaligith <asad2747982@gmail.com>
2025-07-23 04:50:36 +0500

    question 02 to 04 completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Question NO, 1 Exercises Start
+// <-------------Question NO, 1 Exercises Start------------>
 
 // Excercise 1.
 alert("Hello, World")
@@ -18,6 +18,86 @@ var names = "Asad Ali";
 var greet = "Hello " + names ;
 alert(greet)
 
-// Question No, 1 End
+// <--------------Question No, 1 End----------------->
 
-// Question NO, 2 Excersices Start
+// <-----------Question NO, 2 ----------------------->
+
+// Excersices Veriable for String : Declaring and using Text Data Start
+
+// Execercise NO, 01
+var userName = "Asad Ali";
+console.log(userName)
+
+// Ex NO, 02 : Store a welcome message in a Veriable and alert it
+
+var message = "Welcome Asad Ali";
+alert(message)
+
+// Ex NO, 03 Combine first name and last name into one veriable
+
+let first_name = "Asad";
+let last_name = "Ali" ;
+let full_name = first_name + " " + last_name ;
+console.log(full_name)
+
+// Ex No, 04 Store your favorite quote in a variable.
+
+let quote = "code ko readable aur understandable banao, sirf machine ke liye nahi dusre developers ke liye bhi.";
+console.log(quote)
+
+// Ex NO, 05  Create a variable city and show <Welcome to [city]!= in an alert
+
+let city = "Karachi";
+alert(`welcome to ${city}`)
+
+// <----------------Question 02 Excercises End--------------->
+
+// <----------------Question NO, 03 Excercises Start----------->
+//  Variables for Numbers: Storing and Using Numerical Data
+
+// Ex No, 01  Declare a variable age and assign your age to it.
+let age = 27;
+
+// Ex NO, 02 Store two numbers in variables and add them.
+let first_nbr = 8;
+let second_nbr = 5;
+console.log(first_nbr + second_nbr)
+
+// Ex NO,03 Subtract one number from another.
+console.log(first_nbr - second_nbr)
+
+// Ex NO, 04 Multiply two numbers stored in variables.
+console.log(first_nbr * second_nbr)
+
+// Ex NO, 05 Show a message like: <You are 20 years old.
+console.log(`you are ${20} years old`)
+
+//<-----------Question NO, 03 End ---------------------->
+
+//<.............Question NO, 04 Start...................>
+
+// 4.Math Expressions: Performing Arithmetic Operations
+
+// Ex, NO, 01  Create two variables: x = 10, y = 5  add them and show result
+let x = 10;
+let y = 5;
+let add = x + y;
+console.log(add)
+
+// Ex, 02 Subtract y from x and alert the answer
+let subtract = y - x;
+console.log(subtract)
+ 
+//Ex 03 Multiply x and y, and log result to the console.
+let multiply = x * y
+console.log(multiply)
+//Ex 04 Divide x by y
+let divide = x / y
+console.log(divide)
+
+// Ex 05  Find the remainder of x % y.
+
+let remainder = x % y;
+console.log(remainder)
+
+// <.....................Question 04 End.................>

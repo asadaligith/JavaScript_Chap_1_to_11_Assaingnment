ccb449ff16dbe7d87979762a3ecc1ea0fe290e63
asadaligith <asad2747982@gmail.com>
2025-07-22 18:35:20 +0500

    question 1 Excersice complete

---
+++ app.js
@@ -0,0 +1,23 @@
+// Question NO, 1 Exercises Start
+
+// Excercise 1.
+alert("Hello, World")
+
+// Excercise 2.
+alert("My Name is Asad Ali")
+
+// Excercise 3.
+alert(5)
+
+// Excercise 4.
+var message = "Practice make Perfect";
+alert(message)
+ 
+// Excercise 5.
+var names = "Asad Ali";
+var greet = "Hello " + names ;
+alert(greet)
+
+// Question No, 1 End
+
+// Question NO, 2 Excersices Start

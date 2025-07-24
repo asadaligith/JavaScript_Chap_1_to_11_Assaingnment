c2142b800e83d382d4b1985fb55545d3726abc69
asadaligith <asad2747982@gmail.com>
2025-07-25 04:14:25 +0500

    completed

--- app.js
+++ app.js
@@ -101,3 +101,94 @@ let remainder = x % y;
 console.log(remainder)
 
 // <.....................Question 04 End.................>
+
+// <--------------------Question 05 Start --------------->
+
+// 5.Prompt: Getting Input from the User
+
+// Ex NO, 01 Ask the users name and greet them.
+let user_names = prompt("Enter Your Name");
+alert("Hello " + user_names)
+
+// Ex No, 02 Ask the users age and display it.
+let user_age = prompt("Enter Your age");
+alert("You are " + user_age + "years Old")
+
+// Ex No, 03 Ask the user for a number, double it, and show the result
+
+let user_number = prompt("Enter a Number");
+let double = user_number * 2;
+alert(`your number is ${user_number} and double is ${double}`)
+
+// Ex NO, 04 Ask for favorite colour and show a message like: 
+let fvt_color = prompt("What is your Favorite color");
+alert(`Wow! I Like ${fvt_color} too!`)
+
+// Ex NO, 05  Ask for city name, and say: <You are from [city].
+let city_name = prompt("What is Your City Name");
+alert(`you are from ${city_name}`)
+
+//<........................Question 05 End ..............>
+
+// <********************Question 06 Start*****************>
+//6.If Statement and Comparison Operators: Controlling Flow
+
+// Ex No,01  Ask user age. If age < 30, show <You are still young man.
+
+let users_ages = Number(prompt("What is your age"));
+
+if (users_ages < 30){
+    alert("You are still Young man")
+} else{
+    alert("You are not a young man ")
+}
+
+// Ex NO, 02  Ask for a number. If its even, show <Even number.
+
+let even_number = Number(prompt("Enter a Number"));
+if (even_number % 2 === 0){
+    alert(`its a ${even_number} Even Number`)
+} else{
+    alert(`its ${even_number} not even Number`)
+}
+
+// Ex No, 03 Compare two numbers and show which one is greater
+
+let Num_one = Number(prompt("Enter first Number to compare "));
+let Num_two = Number(prompt("Enter second Number"));
+
+if (Num_one > Num_two){
+    alert(`${Num_one} is Greater the ${Num_two}`)
+} else if (Num_two > Num_one){
+    alert(`${Num_two} is Greater the ${Num_one}`)
+} else {
+    alert("Both Number is Equal")
+}
+
+// Ex NO,04 Ask the user for password. If correct, say <Access granted
+let password = 12345;
+let user_password = Number(prompt("Enter a Password: ", "Like this 12345"));
+if (user_password === password){
+    alert("Access Granted")
+} else{
+    alert("Access Denaied")
+}
+
+// Ex NO, 05: Use ==, ===, !=, !== to compare two values
+
+let value1 = "5";     // string
+let value2 = 5;       // number
+
+// Loose equality (==): compares value after type conversion
+console.log(value1 == value2);   // true
+
+// Strict equality (===): compares value and type
+console.log(value1 === value2);  // false
+
+// Loose inequality (!=): compares value after type conversion
+console.log(value1 != value2);   // false
+
+// Strict inequality (!==): compares value and type
+console.log(value1 !== value2);  // true
+
+//*****************************            Completed          **************************************88 */
